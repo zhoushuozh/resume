8af43a5dd4b61f869fb0b47ec9d4bd8aeb343dec
zhoushuo <zhoushuozh@gmail.com>
2018-02-21 23:54:40 +0800

    save

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ $(function(){
 		fnHead();
 	})
 })
+
+$(function(){
+	var fbLeft = $(".filter-bar").offset().left;
+
+	$(".filter-btn button").click(function(){
+
+		var fLeft = Math.floor($(this).offset().left-fbLeft);
+		var fWidth = Math.ceil($(this).width());
+		
+		$(".filter-btn button").removeClass("active");
+		$(this).addClass("active");
+
+		$(".filter-bar-line").css({"width":fWidth,"left":fLeft});
+	})
+})
